3eb23619feef152ce5813fd3116f9dc503ab0ecb
kklt92 <kklt92@gmail.com>
2015-04-02 01:38:39 -0400

    add client list

---
+++ chitter.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	//	"strconv"
+)
+
+type Client struct {
+	conn net.Conn
+	ch   chan<- string
+}
+
+func handleConnection(con net.Conn, msgchan chan string, client chan Client) {
+	ch := make(chan string)
+	client <- Client{con, ch}
+	buffer := make([]byte, 4096)
+	for {
+		n, err := con.Read(buffer)
+		if err != nil {
+			//TODO handle error
+			fmt.Println(err)
+			con.Close()
+			break
+		}
+
+		msgchan <- string(buffer[0:n])
+
+	}
+
+}
+
+func handleMsg(msgchan <-chan string, addclient <-chan Client) {
+	clients := make(map[net.Conn]chan<- string)
+	for {
+		select {
+		case msg := <-msgchan:
+			for _, ch := range clients {
+				go func(mch chan<- string) { mch <- "\033" + msg + "\033\r\n" }(ch)
+
+			}
+		case client := <-addclient:
+			clients[client.conn] = client.ch
+		}
+	}
+
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: chitter [port]")
+		return
+	}
+
+	/* Read specific port. */
+	port := os.Args[1]
+	//	if strconv.Atoi(port) > 65535 || strconv.Atoi(port) < 1023 {
+	//		fmt.Println("Port range from 1024 - 65535")
+	//		return
+	//	}
+
+	ln, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Println("cannot listen on " + port + ", please change to another port")
+		fmt.Println(err)
+		return
+	}
+
+	addchan := make(chan Client)
+
+	publicMessages := make(chan string, 10)
+	go handleMsg(publicMessages, addchan)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		go handleConnection(conn, publicMessages, addchan)
+	}
+}
