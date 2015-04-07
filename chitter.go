c5e83dd397ebc260895374e7c7a830b7b8f09bea
kklt92 <kklt92@gmail.com>
2015-04-07 18:13:20 -0400

    add comments

--- chitter.go
+++ chitter.go
@@ -8,23 +8,37 @@ import (
 	"strings"
 )
 
+/**
+ * each client has a Client.
+ */
 type Client struct {
-	conn net.Conn
-	id   int
-	ch   chan<- string
+	conn net.Conn      // connection info
+	id   int           // username
+	ch   chan<- string // output channel
 }
 
+/**
+ * construct message. Using a specific format
+ */
 type Msg struct {
 	msg string
 	src string
 	dst string
 }
 
+/**
+ * handle Connection.
+ * Add client to Client channel. Remove it as well when disconnected
+ */
 func handleConnection(con net.Conn, id int, msgchan chan Msg,
 	addclient chan Client, rmclient chan Client) {
 	ch := make(chan string)
 	client := Client{con, id, ch}
 	addclient <- client
+	defer func() {
+		fmt.Printf("Connection from %v closed.\n", con.RemoteAddr())
+		rmclient <- client
+	}()
 	buffer := make([]byte, 4096)
 
 	welcome := "Welcome to chatroom, your id is " + strconv.Itoa(id) + ".\n"
@@ -33,6 +47,10 @@ func handleConnection(con net.Conn, id int, msgchan chan Msg,
 		fmt.Println(err)
 		con.Close()
 	}
+
+	/**
+	 * always read from connection.
+	 */
 	go func() {
 		for {
 			n, err := con.Read(buffer)
@@ -44,6 +62,7 @@ func handleConnection(con net.Conn, id int, msgchan chan Msg,
 
 			msg := parseMsg(string(buffer[0:n]), id)
 
+			// Add format message into central message channel.
 			msgchan <- formatMsg(msg)
 			/*		msg := <-ch
 					n, err = con.Write([]byte(msg))
@@ -58,20 +77,20 @@ func handleConnection(con net.Conn, id int, msgchan chan Msg,
 		}
 	}()
 
-	go func() {
-		for {
-			msg := <-ch
-			_, err := con.Write([]byte(msg))
-			if err != nil {
-				fmt.Println(err)
-				con.Close()
-				break
-			}
+	// Allways read from its own message channel
+	for {
+		msg := <-ch
+		_, err := con.Write([]byte(msg))
+		if err != nil {
+			fmt.Println(err)
+			con.Close()
+			break
 		}
-	}()
+	}
 
 }
 
+// convert message into standard  Msg format
 func formatMsg(msg Msg) Msg {
 	i := 0
 	for ; msg.msg[i] == ' '; i++ {
@@ -80,8 +99,11 @@ func formatMsg(msg Msg) Msg {
 	return msg
 }
 
+// Parse message and handle the command.
 func parseMsg(msg string, id int) Msg {
 	message := Msg{"", "", ""}
+
+	// Check if there are numbers at begin.
 	i := 0
 	for i = 0; msg[i] >= '0' && msg[i] <= '9'; i++ {
 	}
@@ -94,6 +116,7 @@ func parseMsg(msg string, id int) Msg {
 		return message
 	}
 
+	// If the message with command: ALL
 	if len(msg) >= 4 {
 		if strings.EqualFold(msg[0:3], "ALL") {
 			for i = 3; msg[i] == ' '; i++ {
@@ -107,6 +130,7 @@ func parseMsg(msg string, id int) Msg {
 		}
 	}
 
+	// If the command is: whoami
 	if len(msg) >= 7 {
 		if msg[0:7] == "whoami:" {
 			message.dst = strconv.Itoa(id)
@@ -116,16 +140,19 @@ func parseMsg(msg string, id int) Msg {
 		}
 	}
 
+	// Treat every message else as ALL
 	message.dst = "ALL"
 	message.src = strconv.Itoa(id)
 	message.msg = msg
 	return message
 }
 
+// Send message to different channel
 func handleMsg(msgchan <-chan Msg, addclient <-chan Client, rmclient <-chan Client) {
 	clients := make(map[int]Client)
 	for {
 		select {
+		// When central channel has message.
 		case msg := <-msgchan:
 			if msg.dst == "ALL" {
 				for _, client := range clients {
@@ -144,9 +171,12 @@ func handleMsg(msgchan <-chan Msg, addclient <-chan Client, rmclient <-chan Clie
 					go func(mch chan<- string) { mch <- "Sorry, target user is offline\n" }(client.ch)
 				}
 			}
+		// When add client to client channel
 		case client := <-addclient:
 			clients[client.id] = client
+		// When remove client from client channel
 		case client := <-rmclient:
+			fmt.Printf("Client %v disconnected\n", client.conn.RemoteAddr())
 			delete(clients, client.id)
 		}
 	}
@@ -159,7 +189,7 @@ func main() {
 		return
 	}
 
-	/* Read specific port. */
+	// Read specific port.
 	port, _ := strconv.Atoi(os.Args[1])
 
 	if port > 65535 || port < 1023 {
@@ -174,6 +204,7 @@ func main() {
 		return
 	}
 
+	// Create add/remove client channel
 	addchan := make(chan Client)
 	rmchan := make(chan Client)
 
